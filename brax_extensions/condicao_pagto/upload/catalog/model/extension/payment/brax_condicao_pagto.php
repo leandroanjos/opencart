8630f41635920048dd445df8f533d7f6f87e65f4
Leandro Anjos <leandro@braxti.com>
2019-02-05 18:05:03 -0200

    Desativa o cache da extensão de condição de pagamento.

--- brax_extensions/condicao_pagto/upload/catalog/model/extension/payment/brax_condicao_pagto.php
+++ brax_extensions/condicao_pagto/upload/catalog/model/extension/payment/brax_condicao_pagto.php
@@ -1,17 +1,21 @@
 <?php
 class ModelExtensionPaymentBraxCondicaoPagto extends Model {
 	public function getPaymentTerms($customer_id) {
+		/*
 		$cache_id = 'brax_condicao_pagto.catalog_' . $customer_id;
 		$payment_term_data = $this->cache->get($cache_id);
 
 		if (!$payment_term_data) {
-			$query = $this->db->query("SELECT * FROM " . DB_PREFIX . "brax_payment_term WHERE customer_id = '" . (int)$customer_id . "' ORDER BY name ASC");
+		*/
+		
+		$query = $this->db->query("SELECT * FROM " . DB_PREFIX . "brax_payment_term WHERE customer_id = '" . (int)$customer_id . "' ORDER BY name ASC");
 
-			$payment_term_data = $query->rows;
+		$payment_term_data = $query->rows;
 
+		/*
 			$this->cache->set($cache_id, $payment_term_data);
 		}
-
+		*/
 		return $payment_term_data;
 	}
 }
