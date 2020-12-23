701397e9775d4527f70027f26a16a41d18f5ae20
Leandro Anjos <leandro@braxti.com.br>
2020-12-23 16:30:19 -0300

    Alteração do template de notificação de novos clientes.

--- upload/catalog/controller/mail/register.php
+++ upload/catalog/controller/mail/register.php
@@ -8,35 +8,25 @@ class ControllerMailRegister extends Controller {
 		$data['text_approval'] = $this->language->get('text_approval');
 		$data['text_service'] = $this->language->get('text_service');
 		$data['text_thanks'] = $this->language->get('text_thanks');
-		$data['button_login'] = $this->language->get('button_login');
 
 		$this->load->model('account/customer_group');
-
+			
 		if (isset($args[0]['customer_group_id'])) {
 			$customer_group_id = $args[0]['customer_group_id'];
 		} else {
 			$customer_group_id = $this->config->get('config_customer_group_id');
 		}
-
+					
 		$customer_group_info = $this->model_account_customer_group->getCustomerGroup($customer_group_id);
-
+		
 		if ($customer_group_info) {
 			$data['approval'] = $customer_group_info['approval'];
 		} else {
 			$data['approval'] = '';
 		}
-
-		$data['login'] = $this->url->link('account/login', 'language=' . $this->config->get('config_language'));
-		$data['store_url'] = HTTP_SERVER;
+			
+		$data['login'] = $this->url->link('account/login', '', true);		
 		$data['store'] = html_entity_decode($this->config->get('config_name'), ENT_QUOTES, 'UTF-8');
-
-		$this->load->model('tool/image');
-
-		if (is_file(DIR_IMAGE . html_entity_decode($this->config->get('config_logo'), ENT_QUOTES, 'UTF-8'))) {
-			$data['logo'] = $this->model_tool_image->resize(html_entity_decode($this->config->get('config_logo'), ENT_QUOTES, 'UTF-8'), $this->config->get('theme_default_image_location_width'), $this->config->get('theme_default_image_cart_height'));
-		} else {
-			$data['logo'] = '';
-		}
 
 		$mail = new Mail($this->config->get('config_mail_engine'));
 		$mail->parameter = $this->config->get('config_mail_parameter');
@@ -50,55 +40,88 @@ class ControllerMailRegister extends Controller {
 		$mail->setFrom($this->config->get('config_email'));
 		$mail->setSender(html_entity_decode($this->config->get('config_name'), ENT_QUOTES, 'UTF-8'));
 		$mail->setSubject(sprintf($this->language->get('text_subject'), html_entity_decode($this->config->get('config_name'), ENT_QUOTES, 'UTF-8')));
-		$mail->setHtml($this->load->view('mail/register', $data));
-		$mail->send();
+		$mail->setText($this->load->view('mail/register', $data));
+		$mail->send(); 
 	}
 
+	public function mask($val, $mask) {
+		$maskared = '';
+		$k = 0;
+		for($i = 0; $i<=strlen($mask)-1; $i++) {
+			if($mask[$i] == '#') {
+				if(isset($val[$k])) $maskared .= $val[$k++];
+			} else {
+				if(isset($mask[$i])) $maskared .= $mask[$i];
+			}
+		}
+		return $maskared;
+	}
+	
 	public function alert(&$route, &$args, &$output) {
 		// Send to main admin email if new account email is enabled
 		if (in_array('account', (array)$this->config->get('config_mail_alert'))) {
 			$this->load->language('mail/register');
-
+			
 			$data['text_signup'] = $this->language->get('text_signup');
 			$data['text_firstname'] = $this->language->get('text_firstname');
 			$data['text_lastname'] = $this->language->get('text_lastname');
 			$data['text_customer_group'] = $this->language->get('text_customer_group');
 			$data['text_email'] = $this->language->get('text_email');
 			$data['text_telephone'] = $this->language->get('text_telephone');
-
+			$data['text_razaosocial'] = $this->language->get('text_razaosocial');
+			$data['text_doc'] = $this->language->get('text_doc');
+			
 			$data['firstname'] = $args[0]['firstname'];
 			$data['lastname'] = $args[0]['lastname'];
-
-			$data['login'] = $this->url->link('account/login', 'language=' . $this->config->get('config_language'));
-			$data['store_url'] = HTTP_SERVER;
-			$data['store'] = html_entity_decode($this->config->get('config_name'), ENT_QUOTES, 'UTF-8');
-
-			$this->load->model('tool/image');
-
-			if (is_file(DIR_IMAGE . html_entity_decode($this->config->get('config_logo'), ENT_QUOTES, 'UTF-8'))) {
-				$data['logo'] = $this->model_tool_image->resize(html_entity_decode($this->config->get('config_logo'), ENT_QUOTES, 'UTF-8'), $this->config->get('theme_default_image_location_width'), $this->config->get('theme_default_image_cart_height'));
-			} else {
-				$data['logo'] = '';
-			}
-
+			$data['razaosocial'] = $args[0]['razaosocial'];
+			
 			$this->load->model('account/customer_group');
-
+			
 			if (isset($args[0]['customer_group_id'])) {
 				$customer_group_id = $args[0]['customer_group_id'];
 			} else {
 				$customer_group_id = $this->config->get('config_customer_group_id');
 			}
-
+			
 			$customer_group_info = $this->model_account_customer_group->getCustomerGroup($customer_group_id);
-
+			
 			if ($customer_group_info) {
 				$data['customer_group'] = $customer_group_info['name'];
 			} else {
 				$data['customer_group'] = '';
 			}
 
+			$doc = $args[0]['doc'];
+
+			if ($doc != null && strlen($doc) > 0) {
+				$textoDoc = preg_replace('/[^0-9]/', '', $doc);
+
+				if (strlen($textoDoc) == 14) {
+					$doc = $this->mask($textoDoc, '##.###.###/####-##');
+				} else if (strlen($textoDoc) == 11) {
+					$doc = $this->mask($textoDoc, '###.###.###-##');
+				}
+			}
+
+			$telefone = $args[0]['telephone'];
+
+			if ($telefone != null && strlen($telefone) > 0) {
+				$textoTelefone = preg_replace('/[^0-9]/', '', $telefone);
+
+				if (strlen($textoTelefone) == 8) {
+					$telefone = $this->mask($textoTelefone, "####-####");
+				} else if (strlen($textoTelefone) == 9) {
+					$telefone = $this->mask($textoTelefone, "####-#####");
+				} else if (strlen($textoTelefone) == 10) {
+					$telefone = $this->mask($textoTelefone, "(##) ####-####");
+				} else if (strlen($textoTelefone) == 11) {
+					$telefone = $this->mask($textoTelefone, "(##) ####-#####");
+				}
+			}
+			
+			$data['doc'] = $doc;
 			$data['email'] = $args[0]['email'];
-			$data['telephone'] = $args[0]['telephone'];
+			$data['telephone'] = $telefone;
 
 			$mail = new Mail($this->config->get('config_mail_engine'));
 			$mail->parameter = $this->config->get('config_mail_parameter');
@@ -112,11 +135,11 @@ class ControllerMailRegister extends Controller {
 			$mail->setFrom($this->config->get('config_email'));
 			$mail->setSender(html_entity_decode($this->config->get('config_name'), ENT_QUOTES, 'UTF-8'));
 			$mail->setSubject(html_entity_decode($this->language->get('text_new_customer'), ENT_QUOTES, 'UTF-8'));
-			$mail->setText($this->load->view('mail/register_alert', $data));
+			$mail->setHtml($this->load->view('mail/register_alert', $data));
 			$mail->send();
 
 			// Send to additional alert emails if new account email is enabled
-			$emails = explode(',', (string)$this->config->get('config_mail_alert_email'));
+			$emails = explode(',', $this->config->get('config_mail_alert_email'));
 
 			foreach ($emails as $email) {
 				if (utf8_strlen($email) > 0 && filter_var($email, FILTER_VALIDATE_EMAIL)) {
@@ -124,6 +147,6 @@ class ControllerMailRegister extends Controller {
 					$mail->send();
 				}
 			}
-		}
+		}	
 	}
-}
+}		
