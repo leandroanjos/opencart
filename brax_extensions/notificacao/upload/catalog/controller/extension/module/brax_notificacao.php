b580eade723e47ba5189d138b6a426e0ad4f1f48
Leandro Anjos <leandro@braxti.com.br>
2022-10-10 15:40:03 -0300

    Correção para evitar alerta nos logs do sistema.

--- brax_extensions/notificacao/upload/catalog/controller/extension/module/brax_notificacao.php
+++ brax_extensions/notificacao/upload/catalog/controller/extension/module/brax_notificacao.php
@@ -9,42 +9,45 @@ class ControllerExtensionModuleBraxNotificacao extends Controller {
 		
 		$url_data = $this->request->get;
 
-		if ($this->config->get('module_brax_notificacao_status')) {			
-			if ($url_data != null && $url_data['type'] != null && $url_data['type'] == 'registro_aprovado') {
-				if ($url_data['email'] != null && $url_data['status'] == '1') {
-					log_message("Notificacao de Registro Aprovado...");
-					$this->load->model('account/customer');
-			
-					$last_email = "";
-					if ($url_data['customer_id'] != null) {
-						$customer_info = $this->model_account_customer->getCustomer($url_data['customer_id']);
+		if ($this->config->get('module_brax_notificacao_status')) {		
+			if ($url_data != null && isset($_POST['type'])) {	
+				if ($url_data['type'] != null && $url_data['type'] == 'registro_aprovado') {
+					if ($url_data['email'] != null && $url_data['status'] == '1') {
+						log_message("Notificacao de Registro Aprovado...");
+						$this->load->model('account/customer');
+				
+						$last_email = "";
+						if ($url_data['customer_id'] != null) {
+							$customer_info = $this->model_account_customer->getCustomer($url_data['customer_id']);
 
-						if ($customer_info) {
-							$last_email = $customer_info['email'];
+							if ($customer_info) {
+								$last_email = $customer_info['email'];
+							}
+						}
+
+						if ($url_data['email'] != $last_email) {
+							log_message('Novo e-mail: ' . $url_data['email']);
+							
+							$subject = sprintf($this->language->get('text_subject'), $store_name);
+
+							$data['text_welcome'] =  sprintf($this->language->get('text_welcome'), $store_name);
+							$data['text_login'] = $this->language->get('text_login');
+							$data['text_redefine'] = $this->language->get('text_redefine');
+							$data['text_service'] = $this->language->get('text_service');
+							$data['text_thanks'] = $this->language->get('text_thanks');
+
+							// Renderiza o email
+							$message = $this->load->view('mail/registro_aprovado', $data);
+							
+							$receiver = $url_data['email'];
+							//$receiver = 'leandro.anjos2@gmail.com';
+							//$message = "Teste no envio de email via middleware...";
+							send_mail($this->config, $receiver, $subject, $message);
+						} else {
+							log_message('E-mail ja cadastrado: ' . $url_data['email']);
 						}
 					}
-
-					if ($url_data['email'] != $last_email) {
-						log_message('Novo e-mail: ' . $url_data['email']);
-						
-						$subject = sprintf($this->language->get('text_subject'), $store_name);
-
-						$data['text_welcome'] =  sprintf($this->language->get('text_welcome'), $store_name);
-						$data['text_login'] = $this->language->get('text_login');
-						$data['text_redefine'] = $this->language->get('text_redefine');
-						$data['text_service'] = $this->language->get('text_service');
-						$data['text_thanks'] = $this->language->get('text_thanks');
-
-						// Renderiza o email
-						$message = $this->load->view('mail/registro_aprovado', $data);
-						
-						$receiver = $url_data['email'];
-						//$receiver = 'leandro.anjos2@gmail.com';
-						//$message = "Teste no envio de email via middleware...";
-						send_mail($this->config, $receiver, $subject, $message);
-					} else {
-						log_message('E-mail ja cadastrado: ' . $url_data['email']);
-					}
+				
 				}
 			}
 		}
